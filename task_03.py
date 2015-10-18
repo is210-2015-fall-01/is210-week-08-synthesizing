20b5e6f03b0a41edf0c991d293a290c2d0f67ca7
Shinseeker <dailymanj88@live.com>
2015-10-18 21:14:21 +0000

    added import decimal to task 3 of week8

--- task_03.py
+++ task_03.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 """w8 synthesizing task 03.py"""
 
+import decimal
 
 NAME = raw_input('What is your name? ')
 
 PRINC = int(raw_input('What is the amount of your principal? '))
 
-YEARS = int(raw_input('For how many years is this loan being borrows '))
+YEARS = int(raw_input('For how many years is this loan being borrowed? '))
 
 PREQUAL = raw_input('Are you pre-qualidied for this loan? ')
 
@@ -16,46 +17,46 @@ RATE = None
 if 0 <= PRINC and PRINC <= 199999:
     if 1 <= YEARS and YEARS <= 15:
         if PREQUAL == 'Yes' or PREQUAL == 'y':
-            RATE = 0.0363
+            RATE = '0.0363'
         elif PREQUAL == 'No' or PREQUAL == 'n':
-            RATE = 0.0465
+            RATE = '0.0465'
     elif 16 <= YEARS and YEARS <= 20:
         if PREQUAL == 'Yes' or PREQUAL == 'y':
-            RATE = 0.0404
+            RATE = '0.0404'
         elif PREQUAL == 'No' or PREQUAL == 'n':
-            RATE = 0.0498
+            RATE = '0.0498'
     elif 21 <= YEARS and YEARS <= 30:
         if PREQUAL == 'Yes' or PREQUAL == 'y':
-            RATE = 0.0577
+            RATE = '0.0577'
         elif PREQUAL == 'No' or PREQUAL == 'n':
-            RATE = 0.0639
+            RATE = '0.0639'
 elif 200000 <= PRINC and PRINC <= 999999:
     if 1 <= YEARS and YEARS <= 15:
         if PREQUAL == 'Yes' or PREQUAL == 'y':
-            RATE = 0.0302
+            RATE = '0.0302'
         elif PREQUAL == 'No' or PREQUAL == 'n':
-            RATE = 0.0398
+            RATE = '0.0398'
     elif 16 <= YEARS and YEARS <= 20:
         if PREQUAL == 'Yes' or PREQUAL == 'y':
-            RATE = 0.0327
+            RATE = '0.0327'
         elif PREQUAL == 'No' or PREQUAL == 'n':
-            RATE = 0.0408
+            RATE = '0.0408'
     elif 21 <= YEARS and YEARS <= 30:
         if PREQUAL == 'Yes' or PREQUAL == 'y':
-            RATE = 0.0466
+            RATE = '0.0466'
 elif 1000000 <= PRINC:
     if 1 <= YEARS and YEARS <= 15:
         if PREQUAL == 'Yes' or PREQUAL == 'y':
-            RATE = 0.0205
+            RATE = '0.0205'
     elif 16 <= YEARS and YEARS <= 20:
         if PREQUAL == 'Yes' or PREQUAL == 'y':
-            RATE = 0.0262
-N = 12.0
-if RATE:
-    TOTAL = round(PRINC * (1 + RATE/N)**(N*YEARS))
-else:
-    TOTAL = None
-    
+            RATE = '0.0262'
+N = 12
+if RATE is not None:
+    RATE = decimal.Decimal(RATE)
+    TOTAL = (PRINC * (1 + RATE/N)**(N*YEARS))
+    TOTAL = int(round(TOTAL))
+
 
 print ''
 print 'Loan Report For: {}'.format(NAME)
@@ -64,7 +65,4 @@ print '      Principle:{:>15}'.format(PRINC)
 print '      Duration:{:>13}'.format(YEARS)+'yrs'
 print '      Pre-qualified?:{:>10}'.format(PREQUAL)
 print ''
-if TOTAL is not None:
-    print '      Total:{:>19.0f}'.format(TOTAL)
-else:
-    print '      Total:{:>19}'.format(TOTAL)
+print '      Total:{:>19.0f}'.format(TOTAL)
