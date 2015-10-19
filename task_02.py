b0b88b63cdcb8d87d8b4590b35e6f77d6a7b32dd
Tresanne <tresannebonnick@hotmail.com>
2015-10-19 07:07:30 +0000

    Tasks

---
+++ task_02.py
@@ -0,0 +1,8 @@
+# /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task02"""
+
+DAY = raw_input('What day is it? ').lower()[:3]
+TIME = int(raw_input('What time is it? '))
+SNOOZE = True if ('sat' or 'sun') and (TIME < 600) else (' Beep! ' * 5)
+print SNOOZE
