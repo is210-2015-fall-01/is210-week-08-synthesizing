e9a818cc46121085e016a154ff95eff5baded836
Grace Park <pjeanne14@gmail.com>
2015-10-19 05:31:50 +0000

    Run GitH.

--- task_03.py
+++ task_03.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """This is a mortgage calculator. Lifetime compound interest of loan."""
@@ -11,21 +10,21 @@ PRINC = int(raw_input('What is the amount of your principal? '))
 DURAT = int(raw_input('For how many years is this loan being borrowed? '))
 PREQU = raw_input('Are you pre-qualified for this loan? ')
 PREQU1 = PREQU[:1]
-PREQU2 = PREQU1.upper()[:1]
+PREQU2 = PREQU1.lower()[:1]
 
 if PRINC < 199999:
     if DURAT <= 15:
-        if PREQU2 == 'Y':
+        if PREQU2 == 'y':
             INTRAT = decimal.Decimal('3.63')
         else:
             INTRAT = decimal.Decimal('4.65')
     elif 16 <= DURAT and DURAT <= 20:
-        if PREQU2 == 'Y':
+        if PREQU2 == 'y':
             INTRAT = decimal.Decimal('4.04')
         else:
             INTRAT = decimal.Decimal('4.98')
     elif 21 <= DURAT and DURAT <= 30:
-        if PREQU2 == 'Y':
+        if PREQU2 == 'y':
             INTRAT = decimal.Decimal('5.77')
         else:
             INTRAT = decimal.Decimal('6.39')
@@ -34,28 +33,29 @@ if PRINC < 199999:
 
 elif 200000 <= PRINC and PRINC <= 999999:
     if DURAT <= 15:
-        if PREQU2 == 'Y':
+        if PREQU2 == 'y':
             INTRAT = decimal.Decimal('3.02')
         else:
             INTRAT = decimal.Decimal('3.98')
     elif 16 <= DURAT and DURAT <= 20:
-        if PREQU2 == 'Y':
+        if PREQU2 == 'y':
             INTRAT = decimal.Decimal('3.27')
         else:
             INTRAT = decimal.Decimal('4.08')
-    elif DURAT >= 21 and PREQU2 == 'Y':
+    elif DURAT >= 21 and PREQU2 == 'y':
         INTRAT = decimal.Decimal('4.66')
     else:
         INTRAT = None  
 
-elif PRINC >= 1000000 and PREQU2 == 'Y':
+elif PRINC >= 1000000 and PREQU2 == 'y':
     if DURAT <= 15:
         INTRAT = decimal.Decimal('2.05')
     elif 16 <= DURAT and DURAT <= 20:
         INTRAT = decimal.Decimal('2.62')
     else:
         INTRAT = None
-
+else:
+    INTRAT = None
 
 if INTRAT is None:
     TOTAL = None
