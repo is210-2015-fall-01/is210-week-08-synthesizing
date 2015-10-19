89efa4318bd1ed7664e4d35e928eee590f68ed6c
Grace Park <pjeanne14@gmail.com>
2015-10-19 04:32:28 +0000

    Task_03's error when doing nosetests. Same 'cannot import name wraps'

--- task_03.py
+++ task_03.py
@@ -2,68 +2,73 @@
 # -*- coding: utf-8 -*-
 """This is a mortgage calculator. Lifetime compound interest of loan."""
 
+import decimal
+
 
 NAME = raw_input('What is your name? ')
-PRINC = raw_input('What is the amount of your principal? ')
-PRINC = int(PRINC)
-DURAT = raw_input('For how many years is this loan being borrowed? ')
-DURAT = int(DURAT)
+PRINC = int(raw_input('What is the amount of your principal? '))
+DURAT = int(raw_input('For how many years is this loan being borrowed? '))
 PREQU = raw_input('Are you pre-qualified for this loan? ')
 PREQU1 = PREQU[:1]
-PREQU2 = PREQU1.upper()
+PREQU2 = PREQU1.upper()[:1]
 
 if PRINC < 199999:
     if DURAT <= 15:
         if PREQU2 == 'Y':
-            intrate = .0363
+            INTRAT = decimal.Decimal('.0363')
         else:
-            intrate = .0465
+            INTRAT = decimal.Decimal('.0465')
     elif 16 <= DURAT and DURAT <= 20:
         if PREQU2 == 'Y':
-            intrate = .0404
+            INTRAT = decimal.Decimal('.0404')
         else:
-            intrate = .0498
+            INTRAT = decimal.Decimal('.0498')
     else:
         if PREQU2 == 'Y':
-            intrate = .0577
+            INTRAT = decimal.Decimal('.0577')
         else:
-            intrate = .0639
+            INTRAT = decimal.Decimal('.0639')
 
 elif 200000 <= PRINC and PRINC <= 999999:
     if DURAT <= 15:
         if PREQU2 == 'Y':
-            intrate = .0302
+            INTRAT = decimal.Decimal('.0302')
         else:
-            intrate = .0398
+            INTRAT = decimal.Decimal('.0398')
     elif 16 <= DURAT and DURAT <= 20:
         if PREQU2 == 'Y':
-            intrate = .0327
+            INTRAT = decimal.Decimal('.0327')
         else:
-            intrate = .0408
+            INTRAT = decimal.Decimal('.0408')
     if DURAT >= 21 and PREQU2 == 'Y':
-        intrate = .0466
+        INTRAT = decimal.Decimal('.0466')
+
 
 elif PRINC >= 1000000 and PREQU2 == 'Y':
     if DURAT <= 15:
-        intrate = .0205
+        INTRAT = decimal.Decimal('.0205')
     elif 16 <= DURAT and DURAT <= 20:
-        intrate = .0262
+        INTRAT = decimal.Decimal('.0262')
 else:
+    INTRAT = None
+
+
+if INTRAT is None:
     TOTAL = None
+else:
+    INTRAT2 = decimal.Decimal(INTRAT) / 100
+    TOTAL = int(round(PRINC * (1 + ((INTRAT2 / 12) ** (DURAT * 12)))))
 
-TOTAL = PRINC*(1 + float((float(intrate)/12))**(DURAT*12))
-TOTAL = int(TOTAL)
-print PRINC
-print DURAT
-print PREQU
-print TOTAL
+REPORT = 'Loan Report for: {}'.format(NAME)
+RPRINC = '      Principal:          ${:>2}'.format(PRINC)
+RDURAT = '      Duration:  {:>13}yrs'.format(DURAT)
+RPREQU = '      Pre-qualified?: {:>11}'.format(PREQU)
+RTOTAL = '      Total               ${:>5}'.format(TOTAL)
 
-
-#REPORT = "'Loan Report for: {}'.format(NAME)
-#'-' * 50
-# 'Principal: ${}'.format(PRINC)
-# 'Duraction: {}yrs'.format{DURAT}
-# 'Pre-qualified?: {}'.format{PREQU}
-#
-# 'Total: $'.format()"
-#print REPORT
+print ''
+print REPORT
+print '-' * 50
+print RPRINC
+print RDURAT
+print RPREQU
+print RTOTAL
