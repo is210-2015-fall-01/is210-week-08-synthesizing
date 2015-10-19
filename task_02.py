f56b4c61561bef72345800044d18b60c75805ca5
Grace Park <pjeanne14@gmail.com>
2015-10-19 05:08:35 +0000

    Possibly fixed.

--- task_02.py
+++ task_02.py
@@ -7,9 +7,11 @@ DAY2 = DAY1[:3]
 DAY1 = DAY2.lower()
 TIME1 = raw_input('What time is it? (4-digit number without colon): ')
 TIME1 = int(TIME1)
+STAY = DAY1 == 'sat' or DAY1 == 'sun'
+TIME2 = TIME1 < 600
 
-if (DAY1 == 'sat' or 'sun') or TIME1 < 600:
+if STAY or TIME2:
     SNOOZE = True
 else:
-    print 'Beep! Beep! Beep! Beep! Beep!'
+    print 'Beep! ' * 5
     SNOOZE = False
