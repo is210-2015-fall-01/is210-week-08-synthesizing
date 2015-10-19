21129f0c25b2bdd0caf936714dfbbebf06df4a06
Grace Park <pjeanne14@gmail.com>
2015-10-19 05:23:09 +0000

    Total calculation is correct.

--- task_03.py
+++ task_03.py
@@ -15,50 +15,52 @@ PREQU2 = PREQU1.upper()[:1]
 if PRINC < 199999:
     if DURAT <= 15:
         if PREQU2 == 'Y':
-            INTRAT = decimal.Decimal('.0363')
+            INTRAT = decimal.Decimal('3.63')
         else:
-            INTRAT = decimal.Decimal('.0465')
+            INTRAT = decimal.Decimal('4.65')
     elif 16 <= DURAT and DURAT <= 20:
         if PREQU2 == 'Y':
-            INTRAT = decimal.Decimal('.0404')
+            INTRAT = decimal.Decimal('4.04')
         else:
-            INTRAT = decimal.Decimal('.0498')
-    else:
+            INTRAT = decimal.Decimal('4.98')
+    elif 21 <= DURAT and DURAT <= 30:
         if PREQU2 == 'Y':
-            INTRAT = decimal.Decimal('.0577')
+            INTRAT = decimal.Decimal('5.77')
         else:
-            INTRAT = decimal.Decimal('.0639')
+            INTRAT = decimal.Decimal('6.39')
+    else:
+        INTRAT = None
 
 elif 200000 <= PRINC and PRINC <= 999999:
     if DURAT <= 15:
         if PREQU2 == 'Y':
-            INTRAT = decimal.Decimal('.0302')
+            INTRAT = decimal.Decimal('3.02')
         else:
-            INTRAT = decimal.Decimal('.0398')
+            INTRAT = decimal.Decimal('3.98')
     elif 16 <= DURAT and DURAT <= 20:
         if PREQU2 == 'Y':
-            INTRAT = decimal.Decimal('.0327')
+            INTRAT = decimal.Decimal('3.27')
         else:
-            INTRAT = decimal.Decimal('.0408')
-    if DURAT >= 21 and PREQU2 == 'Y':
-        INTRAT = decimal.Decimal('.0466')
-
+            INTRAT = decimal.Decimal('4.08')
+    elif DURAT >= 21 and PREQU2 == 'Y':
+        INTRAT = decimal.Decimal('4.66')
+    else:
+        INTRAT = None  
 
 elif PRINC >= 1000000 and PREQU2 == 'Y':
     if DURAT <= 15:
-        INTRAT = decimal.Decimal('.0205')
+        INTRAT = decimal.Decimal('2.05')
     elif 16 <= DURAT and DURAT <= 20:
-        INTRAT = decimal.Decimal('.0262')
-else:
-    INTRAT = None
+        INTRAT = decimal.Decimal('2.62')
+    else:
+        INTRAT = None
 
 
 if INTRAT is None:
     TOTAL = None
 else:
     INTRAT2 = decimal.Decimal(INTRAT) / 100
-    TOTAL = int(round(PRINC * (1 + ((INTRAT2 / 12) ** (DURAT * 12)))))
-
+    TOTAL = int(round(PRINC * ((1 + (INTRAT2 / 12)) ** (DURAT * 12))))
 REPORT = 'Loan Report for: {}'.format(NAME)
 RPRINC = '      Principal:          ${:>2}'.format(PRINC)
 RDURAT = '      Duration:  {:>13}yrs'.format(DURAT)
