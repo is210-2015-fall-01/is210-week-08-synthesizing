d8a85560f5210e2439ddb4926b5eec1e427b6b77
Alex Doler <aedoler@gmail.com>
2015-10-22 01:38:10 +0000

    Updating

--- task_03.py
+++ task_03.py
@@ -8,64 +8,77 @@ import decimal
 
 NAME = raw_input("What is your name? ")
 PRIN = raw_input("What is the amount of your principal? ")
-PRIN = int(PRIN)
+PRIN = decimal.Decimal(PRIN)
 DUR = raw_input("For how many years is this loan being borrowed? ")
-DUR = int(DUR)
+DUR = decimal.Decimal(DUR)
 PREQ_RAW = raw_input("Are you pre-qualified for this loan? ")
 PREQ = PREQ_RAW.lower()
 PREQ = PREQ[0]
 IR = None
 
+
 if PRIN >= 0 and PRIN <= 199999:
     if DUR >= 1 and DUR <= 15:
         if PREQ == 'y':
-            IR = 3.63
+            IR = '3.63'
         elif PREQ == 'n':
-            IR = 4.65
+            IR = '4.65'
     elif DUR >= 16 and DUR <= 20:
         if PREQ == 'y':
-            IR = 4.04
+            IR = '4.04'
         elif PREQ == 'n':
-            IR = 4.98
+            IR = '4.98'
     elif DUR >= 21 and DUR <= 30:
         if PREQ == 'y':
-            IR = 5.77
+            IR = '5.77'
         elif PREQ == 'n':
-            IR = 6.39
+            IR = '6.39'
 elif PRIN >= 200000 and PRIN <= 999999:
     if DUR >= 1 and DUR <= 15:
         if PREQ == 'y':
-            IR = 3.02
+            IR = '3.02'
         elif PREQ == 'n':
-            IR = 3.98
+            IR = '3.98'
     elif DUR >= 16 and DUR <= 20:
         if PREQ == 'y':
-            IR = 3.27
+            IR = '3.27'
         elif PREQ == 'n':
-            IR = 4.08
+            IR = '4.08'
     elif DUR >= 21 and DUR <= 30:
         if PREQ == 'y':
-            IR = 4.66
+            IR = '4.66'
 elif PRIN >= 1000000:
     if DUR >= 1 and DUR <= 15:
         if PREQ == 'y':
-            IR = 2.05
+            IR = '2.05'
     elif DUR >= 16 and DUR <= 20:
         if PREQ == 'y':
-            IR = 2.62
+            IR = '2.62'
 
-if IR is None:
-    TOTAL = None
+if IR:
+    pass
 else:
-    IR = decimal.Decimal(IR) / 100
-    TOTAL = int(round(PRIN * ((1 + (IR / 12)) ** (12 * DUR))))
+    IR = 0
+    TOTAL = 0
+
+IR = decimal.Decimal(IR)
+IR = IR / decimal.Decimal('100')
+ONE = decimal.Decimal('1')
+MONTH = decimal.Decimal('12')
+
+TOTAL = int(round(PRIN * ((ONE + (IR / MONTH))**(MONTH * DUR))))
+# TOTAL = (round(TOTAL)
+
 
 REPORT = 'Loan Report for: {}\n\
 --------------------------------------------------------------------\n\
-\t{:<15} {:>12,}\n\
+\t{:<15} {:12,}\n\
 \t{:<15} {:>12}\n\
 \t{:<15} {:>12}\n\n\
 \t{:<15} {:>12,}'
 
-print REPORT.format(NAME, 'Principal:', PRIN, 'Duration:', str(DUR) + 'yrs',
+
+
+print REPORT.format(NAME, 'Principal:', PRIN, 'Duration:', str(DUR) + 'yrs', \
                     'Pre-Qualified?:', PREQ_RAW, 'Total:', TOTAL)
+
